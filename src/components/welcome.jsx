ded4d7e2eaa7333b77edb7e9b6246e833a7a80e5
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-16 02:12:32 +0300

    Update welcome.jsx

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 const Welcome = () => (
-  <Section>
-    <h1>
+  <Section className="flex flex-col justify-center items-center">
+    <h1 className="text-2xl">
       I’m Salim
       <br />
       Glad to see you!
     </h1>
-    <p>
+    <p className="text-lg">
       I’m a software developer! I can help you build a product ,
       feature or website Look through some of my work and experience!
       If you like what you see and have a project you need coded, don’t hesitate to contact me.
