77b03de0106603bf81db50391bd7408249dfe769
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-15 21:58:01 +0300

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,18 @@
+import styled from 'styled-components';
+
 const Navbar = () => (
-  <section className="flex space-x">
+  <Section className="flex space-x">
     <div className="">
       <h1>logo</h1>
     </div>
     <nav>
       <h2 href="#">nav</h2>
     </nav>
-  </section>
+  </Section>
 );
 
 export default Navbar;
+
+const Section = styled.section`
+
+`;
