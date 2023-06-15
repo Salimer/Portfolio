5506d4afa928dc375e50602e283b98b1f5ccbc25
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-15 16:33:40 +0300

    Update navbar.jsx

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+
+const Navbar = () => (
+  <section className="flex space-x">
+    <div className="">
+      <h1>logo</h1>
+    </div>
+    <nav>
+      <h2 href="#">nav</h2>
+    </nav>
+  </section>
+);
+
+export default Navbar;
