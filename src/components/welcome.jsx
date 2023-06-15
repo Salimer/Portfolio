9516ba69bb340f316350fce32f760647af464031
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-16 01:53:33 +0300

    Update welcome.jsx

---
+++ src/components/welcome.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const Welcome = () => (
+  <Section>
+    <h1>
+      I’m Salim
+      <br />
+      Glad to see you!
+    </h1>
+    <p>
+      I’m a software developer! I can help you build a product ,
+      feature or website Look through some of my work and experience!
+      If you like what you see and have a project you need coded, don’t hesitate to contact me.
+    </p>
+    <h3>LET’S CONNECT</h3>
+  </Section>
+);
+
+export default Welcome;
+
+const Section = styled.section``;
