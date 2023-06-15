5c1dcaff83f4b340707dbcdd4fc8fce2c8513e61
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-16 01:53:29 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import styled from 'styled-components';
 import './App.css';
 import Navbar from './components/navbar';
+import Welcome from './components/welcome';
 
 function App() {
   return (
-    <Navbar />
+    <Container>
+      <Navbar />
+      <Welcome />
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+
+`;
