8ccda44a24e7f76844dadcbd069828f0ac8a9177
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-16 15:25:04 +0300

    Update welcome.jsx

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,18 +1,19 @@
 import styled from 'styled-components';
 
+const borderRadiusValue = '0 0 0 4rem';
 const Welcome = () => (
-  <Section className="flex flex-col justify-center items-center mt-40">
+  <Section className="p-6 flex flex-col justify-center items-center pt-40 bg-white h-screen">
     <h1 className="text-4xl text-left w-full font-bold leading-relaxed">
       I’m Salim.
       <br />
       Glad to see you!
     </h1>
-    <p className="text-lg text-left">
+    <p className="text-lg text-left py-4">
       I’m a software developer! I can help you build a product ,
       feature or website Look through some of my work and experience!
       If you like what you see and have a project you need coded, don’t hesitate to contact me.
     </p>
-    <div className="w-full py-4">
+    <div className="w-full">
       <h3 className="text-logoColor">LET’S CONNECT</h3>
       <div className="flex gap-4 my-2">
         <ion-icon size="large" name="logo-github" />
@@ -27,7 +28,13 @@ const Welcome = () => (
 export default Welcome;
 
 const Section = styled.section`
-ion-icon {
+  border-radius: ${(props) => props.borderRadius};
+
+  ion-icon {
     color: #505F79;
   }
 `;
+
+Section.defaultProps = {
+  borderRadius: borderRadiusValue,
+};
