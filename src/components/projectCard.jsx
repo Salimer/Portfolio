82b1aecd062a3b509ed90f6f5f005d445dd943e9
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-17 02:57:16 +0300

    Update projectCard.jsx

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -3,21 +3,30 @@ import PropTypes from 'prop-types';
 
 const ProjectCard = ({ project }) => (
   <Li className="m-6 p-4 flex flex-col bg-white gap-3">
-    <img src={project.image} className="w-full h-56 rounded-lg" alt={project.name} />
-    <h2>{project.name}</h2>
     <div>
-      <span>{project.type[0]}</span>
+      <img src={project.image} className="w-full h-52 rounded-lg" alt={project.name} />
+    </div>
+    <h2 className="text-logoColor4">{project.name}</h2>
+    <div>
+      <span className="text-logoColor2 font-semibold">{project.type[0]}</span>
       <ion-icon name="ellipse" />
-      <span>{project.type[1]}</span>
+      <span className="text-projectCardText font-semibold">{project.type[1]}</span>
       <ion-icon name="ellipse" />
-      <span>{project.type[2]}</span>
+      <span className="text-projectCardText font-semibold">{project.type[2]}</span>
     </div>
     <p>{project.description}</p>
-    <div>
-      <span>{project.technology[0]}</span>
-      <span>{project.technology[1]}</span>
-      <span>{project.technology[2]}</span>
+    <div className="flex gap-2 items-center flex-wrap">
+      <span className="bg-logoColor6 px-4 py-1 rounded-lg text-sm text-logoColor font-medium tracking-wide">
+        {project.technology[0]}
+      </span>
+      <span className="bg-logoColor6 px-4 py-1 rounded-lg text-sm text-logoColor font-medium tracking-wide">
+        {project.technology[1]}
+      </span>
+      <span className="bg-logoColor6 px-4 py-1 rounded-lg text-sm text-logoColor font-medium tracking-wide">
+        {project.technology[2]}
+      </span>
     </div>
+    <button type="button" className="w-fit p-4 rounded-lg my-5 border-2 border-logoColor2 tracking-wide text-logoColor2">See project</button>
   </Li>
 );
 
@@ -40,10 +49,6 @@ const Li = styled.li`
     box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.2);
     overflow: hidden;
     transition: all 0.3s ease-in-out;
-    &:hover {
-        box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.5);
-        transform: scale(1.05);
-    }
     h2 {
         font-size: 1.5rem;
         font-weight: 700;
