4392361096d91a3bfedf64492b65dfa25a046c7f
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-16 14:30:14 +0300

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,48 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import { ReactComponent as Logo } from '../assets/logo.svg';
 
-const Navbar = () => (
-  <Section className="flex justify-between">
-    <div className="">
-      <h1>logo</h1>
-    </div>
-    <nav>
-      <h2 href="#">nav</h2>
-    </nav>
-  </Section>
-);
+const Navbar = () => {
+  const Links = [
+    { name: 'HOME', link: '/' },
+    { name: 'SERVICE', link: '/' },
+    { name: 'ABOUT', link: '/' },
+    { name: "BLOG'S", link: '/' },
+    { name: 'CONTACT', link: '/' },
+  ];
+
+  const [open, setOpen] = useState(false);
+  return (
+    <Section className="shadow-md w-full fixed top-0 left-0">
+      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+        <div className="font-bold text-2xl cursor-pointer flex items-center text-gray-800">
+          <span className="text-3xl text-indigo-600 mr-1 pt-2">
+            <Logo className="rounded-full w-9 h-9" />
+          </span>
+        </div>
+
+        <button type="button" onClick={() => setOpen(!open)} className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden">
+          <ion-icon name={open ? 'close' : 'menu'} />
+        </button>
+
+        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ' : 'top-[-490px]'}`}>
+          {
+          Links.map((link) => (
+            <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
+              <a href={link.link} className="text-gray-800 hover:text-gray-400 duration-500">{link.name}</a>
+            </li>
+          ))
+        }
+        </ul>
+      </div>
+    </Section>
+  );
+};
 
 export default Navbar;
 
 const Section = styled.header`
-
+ion-icon {
+  color: #6936f5;
+}
 `;
