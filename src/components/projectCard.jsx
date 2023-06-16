9bf1ece19c0f22544c79635a58bc08142b4e997c
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-16 17:09:07 +0300

    Update projectCard.jsx

---
+++ src/components/projectCard.jsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const ProjectCard = ({ project }) => (
+  <Li>
+    <img src={project.image} alt={project.name} />
+    <h2>{project.name}</h2>
+    <p>{project.description}</p>
+  </Li>
+);
+
+export default ProjectCard;
+
+ProjectCard.propTypes = {
+  project: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+const Li = styled.li`
+    border-radius: 0.5rem;
+    box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.2);
+    overflow: hidden;
+    transition: all 0.3s ease-in-out;
+    &:hover {
+        box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.5);
+        transform: scale(1.05);
+    }
+    img {
+        width: 100%;
+    }
+    h2 {
+        font-size: 1.5rem;
+        font-weight: 700;
+        margin: 0.5rem 0;
+    }
+    p {
+        font-size: 1rem;
+        font-weight: 400;
+        margin: 0.5rem 0;
+    }
+`;
