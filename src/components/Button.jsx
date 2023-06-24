59217a2a3a5371fdede8e5fc70d48e92979b603b
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-24 21:47:11 +0300

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ children }) => (
-  <button type="button" className="w-fit p-4 rounded-lg my-5 border-2 border-logoColor2 tracking-wide text-logoColor2 bg-white">
+const Button = ({ children, button }) => (
+  <button type={button ? 'button' : 'submit'} className="w-fit p-4 rounded-lg my-5 border-2 border-logoColor2 tracking-wide text-logoColor2 bg-white">
     {children}
   </button>
 );
-export default Button;
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
+  button: PropTypes.bool.isRequired,
 };
+
+export default Button;
