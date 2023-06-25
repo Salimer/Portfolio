3eb18f24a970e6e9940c0b4488b3bfdaa80e790c
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-25 16:09:51 +0300

    Update projects.jsx

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 import ProjectCard from './projectCard';
 import bookstore from '../assets/projects/bookstore.png';
 import commentdish from '../assets/projects/commentDish.png';
+import spaceHub from '../assets/projects/spaceHub.png';
+import RLCS from '../assets/projects/RLCS.png';
+import mathmagicians from '../assets/projects/mathmagicians.png';
 
 const projects = [
   {
@@ -21,6 +24,33 @@ const projects = [
     technology: ['HTML', 'CSS', 'JS'],
     image: commentdish,
     link: 'https://github.com/Salimer/COMMENTDISH-project',
+  },
+  {
+    id: 3,
+    name: 'Space-Travelers-Hub',
+    type: ['SALIMER', 'Front End Dev', '2023'],
+    description: 'Space-Travelers-Hub is a web application for a company that provides commercial and scientific space travel services. Implementing React/Redux, the application will allow users to book rockets and join selected space missions.',
+    technology: ['React', 'Redux', 'Style-Components', 'React-Testing-Library', 'React/Bootstrap'],
+    image: spaceHub,
+    link: 'https://github.com/Salimer/Space-Travelers-Hub',
+  },
+  {
+    id: 4,
+    name: 'RLCS-event-template',
+    type: ['SALIMER', 'Front End Dev', '2023'],
+    description: 'RLCS event webpage is a project to market, sell tickets and more about a hypotetical event of Rocket League. This project is adaptable for other topics! Built with HTML, CSS and JavaScript.',
+    technology: ['HTML', 'CSS', 'JavaScript'],
+    image: RLCS,
+    link: 'https://github.com/Salimer/RLCS-event-template',
+  },
+  {
+    id: 5,
+    name: 'Math-magicians',
+    type: ['SALIMER', 'Front End Dev', '2023'],
+    description: 'Math magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: Make simple calculations, and read a random quote. By building this application, I improve my skills in using React.',
+    technology: ['React', 'SCSS'],
+    image: mathmagicians,
+    link: 'https://github.com/Salimer/Math-magicians',
   },
 ];
 
