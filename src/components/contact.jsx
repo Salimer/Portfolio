4a579b6637ebe188e8ffa9df8a0777cda073d2f7
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-25 13:34:35 +0300

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 import Button from './Button';
+import contactShapes from '../assets/contact-shapes.svg';
 
 const Contact = () => {
   const [name, setName] = useState('');
@@ -16,41 +17,43 @@ const Contact = () => {
 
   return (
     <Section id="contact" className="bg-white">
-      <article className="contact p-6 bg-logoColor flex flex-col items-center pt-24 ">
-        <h1 className="text-white text-4xl w-full font-bold leading-relaxed text-center max-w-lg">Contact me</h1>
-        <p className="text-lg text-center py-4 text-white max-w-lg">
-          If you have an application you are interested in developing,
-          a feature that you need built or a project that needs coding.
-          I’d love to help with it!
-        </p>
-      </article>
-      <form
-        className="flex flex-col items-center bg-logoColor p-6"
-        action="https://formspree.io/f/myyangkd"
-        method="POST"
-      >
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          onChange={(e) => setName(e.target.value)}
-          className="p-4 rounded-lg my-2 border-2 border-logoColor2 tracking-wide text-logoColor2 bg-white w-full max-w-lg"
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-          className="p-4 rounded-lg my-2 border-2 border-logoColor2 tracking-wide text-logoColor2 bg-white w-full max-w-lg"
-        />
-        <textarea
-          name="message"
-          placeholder="Message"
-          onChange={(e) => setMessage(e.target.value)}
-          className="p-4 rounded-lg my-2 border-2 border-logoColor2 tracking-wide text-logoColor2 bg-white w-full h-44 max-w-lg"
-        />
-        <Button handleClick={handleForm}>Get in touch</Button>
-      </form>
+      <div className="contact bg-logoColor">
+        <article className="p-6 flex flex-col items-center pt-24 ">
+          <h1 className="text-white text-4xl w-full font-bold leading-relaxed text-center max-w-lg">Contact me</h1>
+          <p className="text-lg text-center py-4 text-white max-w-lg">
+            If you have an application you are interested in developing,
+            a feature that you need built or a project that needs coding.
+            I’d love to help with it!
+          </p>
+        </article>
+        <form
+          className="flex flex-col items-center p-6"
+          action="https://formspree.io/f/myyangkd"
+          method="POST"
+        >
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={(e) => setName(e.target.value)}
+            className="p-4 rounded-lg my-2 border-2 border-logoColor2 tracking-wide text-logoColor2 bg-white w-full max-w-lg"
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            className="p-4 rounded-lg my-2 border-2 border-logoColor2 tracking-wide text-logoColor2 bg-white w-full max-w-lg"
+          />
+          <textarea
+            name="message"
+            placeholder="Message"
+            onChange={(e) => setMessage(e.target.value)}
+            className="p-4 rounded-lg my-2 border-2 border-logoColor2 tracking-wide text-logoColor2 bg-white w-full h-44 max-w-lg"
+          />
+          <Button handleClick={handleForm}>Get in touch</Button>
+        </form>
+      </div>
     </Section>
   );
 };
@@ -60,6 +63,7 @@ export default Contact;
 const Section = styled.section`
     .contact {
         border-radius: 4rem 0 0 0;
+        background-image: url(${contactShapes});
     }
 
     button {
