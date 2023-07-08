5c70c8e55dcbd4baccdb5f22e89b094867b8dab8
Salimer <52242629+Salimer@users.noreply.github.com>
2023-07-09 02:34:34 +0300

    Fix mobile menu length

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
           <ion-icon name={open ? 'close' : 'menu'} />
         </button>
 
-        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ' : 'top-[-490px]'}`}>
+        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-16 ' : 'top-[-490px]'}`}>
           {
           Links.map((link) => (
             <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
