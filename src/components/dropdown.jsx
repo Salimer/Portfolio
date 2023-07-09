1b4242cb5a48927a2012117e6f41eea33207125d
Salimer <52242629+Salimer@users.noreply.github.com>
2023-07-09 23:42:41 +0300

    Add dropdown animations when opening and close

--- src/components/dropdown.jsx
+++ src/components/dropdown.jsx
@@ -1,31 +1,55 @@
+import { useRef } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const DropdownMenu = ({
   title, items, isOpen, toggleDropdown,
 }) => {
+  const arrowRef = useRef(null);
+  // const openRef = useRef(false);
+
   const handleItemClick = (item) => {
     // Do something with the selected item
     console.log(item);
     // Close the dropdown
   };
 
+  const handleDropdownClick = () => {
+    console.log(isOpen);
+    const arrowElement = arrowRef.current;
+    if (arrowElement && arrowElement.classList) {
+      if (isOpen) {
+        arrowElement.classList.remove('clicked');
+      } else {
+        arrowElement.classList.add('clicked');
+      }
+    }
+    // openRef.current = !isOpen;
+    toggleDropdown();
+  };
+
+  if (arrowRef.current && isOpen) {
+    arrowRef.current.classList.add('clicked');
+  } else if (arrowRef.current) {
+    arrowRef.current.classList.remove('clicked');
+  }
+
   return (
     <Dropdown className="w-full">
       <button
         type="button"
         className="flex justify-between items-center w-full my-5 tracking-wide"
-        onClick={toggleDropdown}
+        onClick={handleDropdownClick}
         onKeyDown={(e) => {
           if (e.key === 'Enter') {
-            toggleDropdown();
+            handleDropdownClick();
           }
         }}
         aria-expanded={isOpen}
       >
         <span>{title}</span>
-        <span className="pr-5">
-          <ion-icon name="chevron-down-outline" />
+        <span ref={arrowRef} id="arrow" className="pr-5">
+          <ion-icon name="chevron-back-outline" />
         </span>
       </button>
       {isOpen && (
@@ -33,7 +57,8 @@ const DropdownMenu = ({
           <ul>
             {items.map((item) => (
               <li
-                className="p-5 my-2 text-logoColor2 bg-bodyBackground rounded-lg"
+                id="dropdown-item"
+                className="p-5 my-2 text-logoColor2 bg-bodyBackground rounded-lg w-full"
                 key={item}
                 onClick={() => handleItemClick(item)}
                 onKeyDown={(e) => {
@@ -57,8 +82,35 @@ const DropdownMenu = ({
 export default DropdownMenu;
 
 const Dropdown = styled.section`
-    
-    `;
+#dropdown-item {
+  animation-name: slide-in;
+  animation-duration: 1s;
+  animation-timing-function: ease;
+  animation-delay: 0s;
+  animation-iteration-count: 1;
+  animation-direction: normal;
+  animation-fill-mode: forwards;
+}
+
+@keyframes slide-in {
+  0% {
+    opacity: 0;
+    transform: translateY(-100%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+
+  #arrow {
+    transition: transform 0.3s ease-in-out;
+  }
+  .clicked {
+    transform: rotate(-90deg);
+  }
+`;
 
 DropdownMenu.propTypes = {
   title: PropTypes.string.isRequired,
